44cd0e3b7644ac504c5e54c70e4f5e48b90e1750
yvoigt97 <54365890+yvoigt97@users.noreply.github.com>
2019-08-29 15:38:14 +0200

    Update python-test.php

--- python-test.php
+++ python-test.php
@@ -1,12 +1,12 @@
 <?php
-$uri = "https://berlinonbike.de";
+$uri = " ";
 $start_date = "2019-05-01";
 $end_date = "2019-05-30";
 echo "<h3>";
 echo $uri." ".$start_date." ".$end_date;
 echo "</h3><br>";
 exec("python3 --version 2>&1", $out, $result);
-exec("python3 /www/htdocs/w010903c/gsc.digitalagenten.com/GSC-API-master/search_analytics_api_sample.py ".$uri." ".$start_date." ".$end_date." 2>&1", $out, $result);
+exec("python3 /*path to file ".$uri." ".$start_date." ".$end_date." 2>&1", $out, $result);
 echo "Returncode: " .$result ."<br>";
 echo "Ausgabe des Scripts: " ."<br>";
 echo "<pre>"; print_r($out);
